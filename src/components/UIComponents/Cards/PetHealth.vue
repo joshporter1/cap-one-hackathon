5cb91797a4b461e4a047fb3be94f9281fb7954c0
Joshua Porter <joshporter1@gmail.com>
2017-11-05 10:32:36 -0500

    Added functionality to pet profile card

--- src/components/UIComponents/Cards/PetHealth.vue
+++ src/components/UIComponents/Cards/PetHealth.vue
@@ -3,34 +3,64 @@
    
     
 <div class="row">
-       
     <div class="col-md-4"></div>
     <div class="col-md-4 center-block" id="imagesMain">
-        <p style = "color:#dd3920;" v-if="normal">Happiness Up!</p>
-        <img width="50"   src="static/img/health.png">
-        <img width="50"  src="static/img/health.png">
-        <img width="50"  src="static/img/health.png" v-if="normal">
-       
-        <img width="50"  src="static/img/empty.png" v-if="!normal">
-        <img width="50"  src="static/img/empty.png">
-     
+        <transition name="fade">
+            <p style = "color:#dd3920;" v-if="alert">Happiness Up!</p>
+          </transition>
+        <img width="50"  src="static/img/health.png" v-for="heart in hearts">
+        <img width="50"  src="static/img/empty.png" v-for="heart in empty">
        </div> 
         <div class="col-md-4"></div>
  </div>
-  
-  
 </template>
 
 <script>
   export default {
     name: 'pet-health',
-    props: ['normal']
+    props: ['status'],
+    data () {
+      return {
+        alert: false
+      }
+    },
+    watch: {
+      status () {
+        this.alert = true
+        let _this = this
+        setTimeout(function(){
+          _this.alert = false
+        }, 2500)
+      }
+    },
+    computed: {
+      hearts () {
+        if (this.status == 'sad')
+          return 2
+        if (this.status == 'normal')
+          return 3
+        if (this.status == 'great')
+          return 4
+        else
+          return 2
+      },
+      empty () {
+        return 4 - this.hearts
+      }
+    }
   }
 
 </script>
 
 
 <style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0
+}
+
 .pet-enter-active, .pet-leave-active {
   transition: opacity 5s
 }
