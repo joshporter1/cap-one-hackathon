ca039a555f8686e467f54e69d50f36e40cbc8257
pplum09 <isara.ritthaworn@gmail.com>
2017-11-05 00:20:11 -0400

    added details card to adult page

--- src/components/Dashboard/Views/AdultDash.vue
+++ src/components/Dashboard/Views/AdultDash.vue
@@ -4,9 +4,23 @@
         <user-card>
 
         </user-card>
+        <detail-card></detail-card>
         <members-card>
 
         </members-card>
+      </div>
+      <div class="col-lg-8 col-md-7">
+        <chart-card :chart-data="preferencesChart.data"  chart-type="Pie">
+          <h4 class="title" slot="title">Monthly Spending</h4>
+          <span slot="subTitle"> October 2017</span>
+          <span slot="footer">
+            <i class="ti-timer"></i> Campaign set 2 days ago</span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Open
+            <i class="fa fa-circle text-danger"></i> Bounce
+            <i class="fa fa-circle text-warning"></i> Unsubscribe
+          </div>
+        </chart-card>
       </div>
       <div class="col-lg-8 col-md-7">
         <chart-card :chart-data="preferencesChart.data"  chart-type="Pie">
@@ -28,12 +42,14 @@
   import UserCard from './UserProfile/UserCard.vue'
   import MembersCard from './UserProfile/MembersCard.vue'
   import ChartCard from 'components/UIComponents/Cards/ChartCard.vue'
+  import DetailCard from 'components/UIComponents/Cards/DetailCard.vue'
   export default {
     components: {
       EditProfileForm,
       UserCard,
       MembersCard,
-      ChartCard
+      ChartCard,
+      DetailCard
     },
     data(){
       return {
